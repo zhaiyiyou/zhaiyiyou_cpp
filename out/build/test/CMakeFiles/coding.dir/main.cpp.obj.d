473768b9f04c031b87a3f4362656cbd071d3bda2
zhaiyiyou <zyy5303001gy@outlook.com>
2025-04-25 22:05:01 +0800

    feat: 添加图论算法实现及测试文件
    本次提交新增了图论算法的实现文件 `main.cpp`，并添加了相关的测试文件和文档。具体内容包括：
    - 实现了一个基于 SPFA 算法的最短路径求解器，用于处理带有正负权值的图。
    - 添加了测试日志文件和 CMake 构建系统的相关配置。
    - 新增了文档 `图论\道路和航路.md`，详细描述了算法的输入输出格式及示例。
    
    这些改动为项目提供了图论算法的核心功能，并确保了代码的可测试性和可维护性。

--- out/build/test/CMakeFiles/coding.dir/main.cpp.obj.d
+++ out/build/test/CMakeFiles/coding.dir/main.cpp.obj.d
@@ -134,6 +134,18 @@ CMakeFiles/coding.dir/main.cpp.obj: D:\code1\zhaiyiyou_cpp\main.cpp \
  C:/PROGRA~1/REDPAN~1/mingw64/lib/gcc/x86_64-w64-mingw32/11.4.0/include/c++/bits/ostream.tcc \
  C:/PROGRA~1/REDPAN~1/mingw64/lib/gcc/x86_64-w64-mingw32/11.4.0/include/c++/istream \
  C:/PROGRA~1/REDPAN~1/mingw64/lib/gcc/x86_64-w64-mingw32/11.4.0/include/c++/bits/istream.tcc \
+ C:/PROGRA~1/REDPAN~1/mingw64/lib/gcc/x86_64-w64-mingw32/11.4.0/include/c++/vector \
+ C:/PROGRA~1/REDPAN~1/mingw64/lib/gcc/x86_64-w64-mingw32/11.4.0/include/c++/bits/stl_uninitialized.h \
+ C:/PROGRA~1/REDPAN~1/mingw64/lib/gcc/x86_64-w64-mingw32/11.4.0/include/c++/bits/stl_vector.h \
+ C:/PROGRA~1/REDPAN~1/mingw64/lib/gcc/x86_64-w64-mingw32/11.4.0/include/c++/bits/stl_bvector.h \
+ C:/PROGRA~1/REDPAN~1/mingw64/lib/gcc/x86_64-w64-mingw32/11.4.0/include/c++/bits/vector.tcc \
+ C:/PROGRA~1/REDPAN~1/mingw64/lib/gcc/x86_64-w64-mingw32/11.4.0/include/c++/queue \
+ C:/PROGRA~1/REDPAN~1/mingw64/lib/gcc/x86_64-w64-mingw32/11.4.0/include/c++/deque \
+ C:/PROGRA~1/REDPAN~1/mingw64/lib/gcc/x86_64-w64-mingw32/11.4.0/include/c++/bits/stl_deque.h \
+ C:/PROGRA~1/REDPAN~1/mingw64/lib/gcc/x86_64-w64-mingw32/11.4.0/include/c++/bits/deque.tcc \
+ C:/PROGRA~1/REDPAN~1/mingw64/lib/gcc/x86_64-w64-mingw32/11.4.0/include/c++/bits/stl_heap.h \
+ C:/PROGRA~1/REDPAN~1/mingw64/lib/gcc/x86_64-w64-mingw32/11.4.0/include/c++/bits/stl_queue.h \
+ C:/PROGRA~1/REDPAN~1/mingw64/lib/gcc/x86_64-w64-mingw32/11.4.0/include/c++/bits/uses_allocator.h \
  C:/PROGRA~1/REDPAN~1/mingw64/lib/gcc/x86_64-w64-mingw32/11.4.0/include/c++/cstring \
  C:/PROGRA~1/REDPAN~1/mingw64/x86_64-w64-mingw32/include/string.h \
  C:/PROGRA~1/REDPAN~1/mingw64/x86_64-w64-mingw32/include/sec_api/string_s.h
