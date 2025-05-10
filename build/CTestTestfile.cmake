a578e080e011fbb333a130acbbfb28bb54116095
zhaiyiyou <zyy5303001gy@outlook.com>
2025-05-10 17:36:04 +0800

    chore: 删除构建生成的临时文件和二进制文件
    清理构建过程中生成的临时文件、二进制文件及CMake缓存文件，以保持代码库的整洁。

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/code1/zhaiyiyou_cpp
-# Build directory: D:/code1/zhaiyiyou_cpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- out/build/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/code1/zhaiyiyou_cpp
-# Build directory: D:/code1/zhaiyiyou_cpp/out/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
